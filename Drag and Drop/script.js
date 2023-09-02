1520faac3dcd2be35a0b61a5b6acf4b0e82a7578
Priyadharsini <110250602+Priyadharsini2002@users.noreply.github.com>
2023-09-02 19:37:14 +0530

    Update script.js

--- Drag and Drop/script.js
+++ Drag and Drop/script.js
@@ -1,6 +1,7 @@
 const fill = document.querySelector('.fill')
 const empties = document.querySelectorAll('.empty')
 
+
 fill.addEventListener('dragstart', dragStart)
 fill.addEventListener('dragend', dragEnd)
 
