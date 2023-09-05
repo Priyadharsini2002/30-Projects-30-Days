b19f160506657dd5850b8f4acaabdf0381d21fa4
Priyadharsini <110250602+Priyadharsini2002@users.noreply.github.com>
2023-09-05 19:27:54 +0530

    Update script.js

--- Increment counter/script.js
+++ Increment counter/script.js
@@ -8,7 +8,7 @@ counters.forEach(counter => {
         const c = +counter.innerText
 
         const increment = target / 200
-
+        
         if(c < target) {
             counter.innerText = `${Math.ceil(c + increment)}`
             setTimeout(updateCounter, 1)
